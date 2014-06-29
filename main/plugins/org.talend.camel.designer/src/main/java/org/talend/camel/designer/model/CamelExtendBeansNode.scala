072838c1d7ed4e64d538e28f8d6e740d6a4eef30
Zhangbohong GAO <zgao@talend.com>
2014-06-29 20:43:37 +0800

    translate CamelExtendBeansNode & CamelExtendRoutesNode to scala

---
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/model/CamelExtendBeansNode.scala
@@ -0,0 +1,27 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.camel.designer.model;
+
+import org.talend.camel.designer.util.ECamelCoreImage
+import org.talend.commons.ui.runtime.image.IImage
+import org.talend.core.repository.IExtendRepositoryNode
+
+class CamelExtendBeansNode extends IExtendRepositoryNode {
+
+	override def getNodeImage(): IImage = ECamelCoreImage.BEAN_ICON
+
+	override def getOrdinal() = 16
+
+	override def getChildren(): Array[Object] = Array.empty
+
+}
